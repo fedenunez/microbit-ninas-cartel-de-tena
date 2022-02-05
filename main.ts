f82c83f79d164112f4a09750ef35f402314a1ae1
Federico Nuñez <fedenunezreg@gmail.com>
2022-02-05 09:00:02 -0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,50 @@
+let pause2 = 999
+basic.forever(function () {
+    images.createImage(`
+        . . . . .
+        . # # # .
+        . . # . .
+        . . # . .
+        . . # . .
+        `).showImage(0)
+    basic.pause(pause2)
+    images.createImage(`
+        . # # # .
+        . # . . .
+        . # # . .
+        . # . . .
+        . # # # .
+        `).showImage(0)
+    basic.pause(pause2)
+    images.createImage(`
+        . . . . .
+        . # . . #
+        . # # . #
+        . # . # #
+        . # . . #
+        `).showImage(0)
+    basic.pause(pause2)
+    images.createImage(`
+        . . . . .
+        . . # . .
+        . . . . .
+        . . # . .
+        . . # . .
+        `).showImage(0)
+    basic.pause(pause2)
+    images.createImage(`
+        . . . . .
+        . # # # .
+        . . # . .
+        . . # . .
+        . . # . .
+        `).showImage(0)
+    basic.pause(pause2)
+    images.createImage(`
+        . . . . .
+        . # # # .
+        . # . # .
+        . # # # .
+        . # . # .
+        `).showImage(0)
+})
